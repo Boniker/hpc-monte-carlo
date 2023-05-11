ba057369cca88ea5976c34b12e12c9316e3804ed
Borys Bilkevych <48773101+Boniker@users.noreply.github.com>
2023-05-11 11:24:21 +0300

    Update OPEN_MP-monte-carlo.cpp

--- OPEN_MP-monte-carlo.cpp
+++ OPEN_MP-monte-carlo.cpp
@@ -1,54 +1,55 @@
-#include <iostream>                                                                                                                        
-#include <sstream>                                                                                                                         
-#include <ctime>                                                                                                                           
-#include <omp.h>                                                                                                                           
-#include <cstdlib>                                                                                                                         
-#include <iomanip>                                                                                                                         
-#include <cmath>                                                                                                                           
-#include <ctime>                                                                                                                           
-#include <math.h>                                                                                                                          
+#include <iostream>
+#include <sstream>
+#include <ctime>
+#include <omp.h>
+#include <cstdlib>
+#include <iomanip>
+#include <cmath>
+#include <ctime>
+#include <math.h>
                                                                                                                                            
-using namespace std;                                                                                                                       
+using namespace std;
                                                                                                                                            
-double fRand(double fMin, double fMax)                                                                                                     
+double fRand(double fMin, double fMax)
 {                                                                                                                                          
-    double f = (double)rand() / RAND_MAX;                                                                                                  
-    return fMin + f * (fMax - fMin);                                                                                                       
+    double f = (double)rand() / RAND_MAX;
+    return fMin + f * (fMax - fMin);
 }                                                                                                                                          
                                                                                                                                            
-int main(int argc, char *argv[])                                                                                                           
+int main(int argc, char *argv[])
 {                                                                                                                                          
-    srand(time(NULL));                                                                                                                     
-    double R;                                                                                                                              
-    int N;                                                                                                                                 
-    int numberOfThreads;                                                                                                                   
+    srand(time(NULL));
+    double R;
+    int N;
+    int numberOfThreads;
                                                                                                                                            
-    A:                                                                                                                                     
-    stringstream(argv[1]) >> numberOfThreads;                                                                                              
-    stringstream(argv[2]) >> R;                                                                                                            
-    stringstream(argv[3]) >> N;                                                                                                            
-    cout << "\nnumberOfThreads = " << numberOfThreads << "\nR = " << R << "\nLog(N) = " << N << endl;                                      
-    long precision = pow(2, N);                                                                                                            
-    cout << "Precision (2^N) = " << precision << endl;                                                                                     
+A:
+    stringstream(argv[1]) >> numberOfThreads;
+    stringstream(argv[2]) >> R;
+    stringstream(argv[3]) >> N;
+    cout << "\nnumberOfThreads = " << numberOfThreads << "\nR = " << R << "\nLog(N) = " << N << endl;
+    long precision = pow(2, N);
+    cout << "Precision (2^N) = " << precision << endl;
     omp_set_num_threads(numberOfThreads);                                                                                                  
-    double start = omp_get_wtime();                                                                                                        
-    int m = 0;                                                                                                                             
+    double start = omp_get_wtime();
+    int m = 0;
                                                                                                                                            
-    #pragma omp parallel                                                                                                                   
-    //cout << "Threads number: " << omp_get_num_threads() << endl;                                                                         
-    for (int i = 0; i < precision; ++i)                                                                                                    
+#pragma omp parallel for reduction(+:m)
+    for (int i = 0; i < precision; ++i)
     {                                                                                                                                      
-        double x = fRand(-R, R);                                                                                                           
-        double y = fRand(-R, R);                                                                                                           
-        double z = fRand(-R, R);                                                                                                           
-        if (x * x + y * y + z * z < R * R) m++;                                                                                            
+        double x = fRand(-R, R);
+        double y = fRand(-R, R);
+        double z = fRand(-R, R);
+        if (x * x + y * y + z * z < R * R) m++;
     }                                                                                                                                      
                                                                                                                                            
-    double end = omp_get_wtime();                                                                                                          
-    double time = end - start;                                                                                                             
+double end = omp_get_wtime();
+double time = end - start;
                                                                                                                                            
-    cout << "\n";                                                                                                                          
-    cout << "OmpTime:" << setprecision(8) << time << endl;                                                                                 
-    cout << "Calculation result: " << (double)m / precision * 8 * R * R * R << endl;                                                       
-    cout << "Expected result: " << 4.0 / 3 * M_PI * R * R * R << endl;                                                                     
+cout << "\n";
+cout << "OmpTime:" << setprecision(8) << time << endl;
+cout << "Calculation result: " << (double)m / precision * 8 * R * R * R << endl;
+cout << "Expected result: " << 4.0 / 3 * M_PI * R * R * R << endl;
+                                                                                                                                           
+return 0;
 }
